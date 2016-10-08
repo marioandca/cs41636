a6cd25d4db2cd849b8c709774b15a9d6c8b73f44
Matt Rogers <t.rogers.matthew@gmail.com>
2016-10-08 11:10:41 -0600

    Removed World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("Hello GitHub");
-=======
-	System.out.println("Hello GitHub World");
 	System.out.println("Testing...");
->>>>>>> 9aa8d9b8b24d5f2e3b94884105c6b374c4b5a7c6
     }
 }
