4e9e2eb18a97f0c88a4c95dc4289c6cb43944ef6
Matt Rogers <t.rogers.matthew@gmail.com>
2016-10-22 22:39:08 -0600

    Updated MainActivity.java

--- ScriptRef/app/src/main/java/com/example/scripturereference/MainActivity.java
+++ ScriptRef/app/src/main/java/com/example/scripturereference/MainActivity.java
@@ -38,10 +38,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void display() {
-        System.out.println("IT WORKS!\n IT'S ALIIIVE");
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
